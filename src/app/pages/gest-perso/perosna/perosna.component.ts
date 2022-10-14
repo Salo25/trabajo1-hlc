65705665e96adf5832eb9bfaa66cc47f6b6bb23b
Salo25 <schooldayssal@gmail.com>
2022-10-14 02:32:21 +0200

    Creamos la interfaz Person y añadimos 3 personas por defecto.

--- src/app/pages/gest-perso/perosna/perosna.component.ts
+++ src/app/pages/gest-perso/perosna/perosna.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+
+
+type FullName = {
+  name: string;
+  subname: string;
+};
+type PhoneNum = {
+  countryID: number;
+  phone: number;
+};
+interface Person {
+  id: number;
+  personName: FullName;
+  email: string;
+  age: number;
+  job: string;
+  workExp: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-perosna',
@@ -7,9 +25,41 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['./perosna.component.scss'],
 })
 export class PerosnaComponent implements OnInit {
+  peopleList: Person[];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.peopleList =
+      Array<Person>(
+        {
+          id: 1,
+          personName: { name: 'Elon', subname: 'Mask' },
+          email: 'elonmask@gmail.com',
+          age: 35,
+          job: 'Buisnessman',
+          workExp: 25,
+          description: 'I\'m richest man on the whole world. My mission is improve the humanity using the last technologies.'
+        },
+        {
+          id: 2,
+          personName: { name: 'María', subname: 'García Cueto' },
+          email: 'mariadb@outlook.com',
+          age: 45,
+          job: 'BD Managment',
+          workExp: 27,
+          description: 'I\'m the one in charge of Data Bases from Mercadona.'
+        },
+        {
+          id: 3,
+          personName: { name: 'José', subname: 'Strauss López' },
+          email: 'josegoldengatebridge@yahoo.com',
+          age: 51,
+          job: 'Civil Engineer',
+          workExp: 38,
+          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
+        }
+    );
+  }
 
 }
