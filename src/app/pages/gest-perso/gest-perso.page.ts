d9b62781d9439bbd4a64fb192ece09d8ebb219cb
Salo25 <schooldayssal@gmail.com>
2022-10-14 20:04:19 +0200

    Cambiando el diseño ui/ux de Gestion de personas y añadiendo funcionalidades

--- src/app/pages/gest-perso/gest-perso.page.ts
+++ src/app/pages/gest-perso/gest-perso.page.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FullName, Person } from './perosna/perosna.component';
 
+/*
+export type FullName = {
+  name: string;
+  subname: string;
+};
+export type PhoneNum = {
+  countryID: number;
+  phone: number;
+};
+export interface Person {
+  id: number;
+  personName: FullName;
+  profilePic: string;
+  email: string;
+  age: number;
+  job: string;
+  workExp: number;
+  description: string;
+}
+*/
 @Component({
   selector: 'app-gest-perso',
   templateUrl: './gest-perso.page.html',
   styleUrls: ['./gest-perso.page.scss'],
 })
 export class GestPersoPage implements OnInit {
+  protected peopleList: Person[];
   private atras: Location;
 
 
@@ -17,6 +39,83 @@ export class GestPersoPage implements OnInit {
     this.atras.back();
   }
   ngOnInit() {
+    this.peopleList =
+      Array<Person>(
+        {
+          id: 1,
+          personName: { name: 'Elon', subname: 'Mask' },
+          profilePic: 'https://drive.google.com/uc?export=view&id=1AbVuYe0VdVX3xMLEj7du9XQ0O6HJru2u',
+          email: 'elonmask@gmail.com',
+          age: 35,
+          job: 'Buisnessman',
+          workExp: 25,
+          description: 'I\'m richest man on the whole world. My mission is improve the humanity using the last technologies.'
+        },
+        {
+          id: 2,
+          personName: { name: 'María', subname: 'García Cueto' },
+          profilePic: 'https://drive.google.com/uc?export=view&id=12AUMDitfhUAY1QyZfbE25kfTtzDXjlqW',
+          email: 'mariadb@outlook.com',
+          age: 45,
+          job: 'BD Managment',
+          workExp: 27,
+          description: 'I\'m the one in charge of Data Bases from Mercadona.'
+        },
+        {
+          id: 3,
+          personName: { name: 'José', subname: 'Strauss López' },
+          profilePic: 'https://drive.google.com/uc?export=view&id=1-2xc-qOfpOsUed-5Tx1aiL7SKrprgxaX',
+          email: 'josegoldengatebridge@yahoo.com',
+          age: 51,
+          job: 'Civil Engineer',
+          workExp: 38,
+          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
+        },
+        {
+          id: 4,
+          personName: { name: 'Pepe', subname: 'Strauss López' },
+          profilePic: '',
+          email: 'josegoldengatebridge@yahoo.com',
+          age: 51,
+          job: 'Civil Engineer',
+          workExp: 38,
+          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
+        },
+        {
+          id: 5,
+          personName: { name: 'Pepe', subname: 'Strauss López' },
+          profilePic: '',
+          email: 'josegoldengatebridge@yahoo.com',
+          age: 51,
+          job: 'Civil Engineer',
+          workExp: 38,
+          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
+        },
+        {
+          id: 6,
+          personName: { name: 'Pepe', subname: 'Strauss López' },
+          profilePic: '',
+          email: 'josegoldengatebridge@yahoo.com',
+          age: 51,
+          job: 'Civil Engineer',
+          workExp: 38,
+          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
+        },
+        {
+          id: 7,
+          personName: { name: 'Pepe', subname: 'Strauss López' },
+          profilePic: '',
+          email: 'josegoldengatebridge@yahoo.com',
+          age: 51,
+          job: 'Civil Engineer',
+          workExp: 38,
+          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
+        },
+    );
+  }
+
+  countRows(): number {
+    return Math.ceil( this.peopleList.length / 3 );
   }
 
 }
--- src/app/pages/gest-perso/perosna/perosna.component.ts
+++ src/app/pages/gest-perso/perosna/perosna.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
-type FullName = {
+export type FullName = {
   name: string;
   subname: string;
 };
-type PhoneNum = {
+export type PhoneNum = {
   countryID: number;
   phone: number;
 };
-interface Person {
+export interface Person {
   id: number;
   personName: FullName;
+  profilePic?: string;
   email: string;
   age: number;
-  job: string;
+  job?: string;
   workExp: number;
-  description: string;
+  description?: string;
 }
 
 @Component({
@@ -25,41 +26,10 @@ interface Person {
   styleUrls: ['./perosna.component.scss'],
 })
 export class PerosnaComponent implements OnInit {
-  peopleList: Person[];
+  @Input() person: Person;
 
   constructor() { }
 
-  ngOnInit() {
-    this.peopleList =
-      Array<Person>(
-        {
-          id: 1,
-          personName: { name: 'Elon', subname: 'Mask' },
-          email: 'elonmask@gmail.com',
-          age: 35,
-          job: 'Buisnessman',
-          workExp: 25,
-          description: 'I\'m richest man on the whole world. My mission is improve the humanity using the last technologies.'
-        },
-        {
-          id: 2,
-          personName: { name: 'María', subname: 'García Cueto' },
-          email: 'mariadb@outlook.com',
-          age: 45,
-          job: 'BD Managment',
-          workExp: 27,
-          description: 'I\'m the one in charge of Data Bases from Mercadona.'
-        },
-        {
-          id: 3,
-          personName: { name: 'José', subname: 'Strauss López' },
-          email: 'josegoldengatebridge@yahoo.com',
-          age: 51,
-          job: 'Civil Engineer',
-          workExp: 38,
-          description: 'I\'m one of the most famous engineers in the worl. I built Golden Gate Bridge in San Francisco.'
-        }
-    );
-  }
+  ngOnInit() {}
 
 }
